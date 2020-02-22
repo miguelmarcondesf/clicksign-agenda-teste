e8fa8eedfbf766374714f1802b4eb03c8b314524
Miguel Marcondes <miguelmarcondesfilho@gmail.com>
2020-02-22 13:32:47 -0300

    Adding style for home page with no contact message added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,47 @@
 <template>
   <div class="home">
-    <img
-    id="logo"
-    src="../assets/imgs/ic-logo.svg"
-    alt="Ubook logo with pink coloring"
-    >
-
-    <input
-    type="text"
-    name="search"
-    placeholder="Buscar..."
-    aria-label="Busque pela agenda"
-    >
-
-    <div class="no-contacts-wrapper">
+    <div class="header">
       <img
-      id="book"
-      src="../assets/imgs/ic-book.svg"
-      alt="A opened book illustration with grey coloring"
+      id="logo"
+      src="../assets/imgs/ic-logo.svg"
+      alt="Ubook logo with pink coloring"
       >
 
-      <p class="no-contact-text">Nenhum contato foi criado ainda.</p>
-
-      <button type="button" name="criar-contato">
-        + Criar contato
-      </button>
+      <div class="input-icon">
+        <input
+        type="text"
+        name="search"
+        placeholder="Buscar..."
+        aria-label="Busque pela agenda"
+        >
+        <img
+        src="../assets/imgs/ic-search.svg"
+        alt="">
+      </div>
 
     </div>
+
+    <div class="home-content-wrapper">
+      <div class="no-contacts-wrapper">
+        <img
+        id="book"
+        src="../assets/imgs/ic-book.svg"
+        alt="A opened book illustration with grey coloring"
+        >
+
+        <p class="no-contact-text">Nenhum contato foi criado ainda.</p>
+
+        <button
+        type="button"
+        name="criar-contato"
+        class="add-button"
+        >
+          + Criar contato
+        </button>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -38,3 +53,67 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../assets/atoms/buttons';
+
+.home {
+  height: 100vh;
+
+  .header {
+    max-width: 100vw;
+    max-height: 6.25vh;
+    padding: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    #logo {
+      height: 50%;
+      width: auto;
+    }
+
+    .input-icon {
+      position: relative;
+      display: flex;
+      align-items: center;
+      min-height: 100%;
+
+      input {
+        height: 32px;
+        width: 72vw;
+        border-radius: 4px;
+        background-color: var(--pale-lilac);
+        padding: 0 8px;
+        border: 0;
+      }
+
+      img {
+        position: absolute;
+        right: 8px;
+      }
+    }
+  }
+
+  .home-content-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+
+    .no-contacts-wrapper {
+      text-align: center;
+
+      #book {
+        width: 16.45vw;
+        height: auto;
+      }
+
+      p {
+        margin: 24px 0;
+      }
+    }
+  }
+}
+</style>
